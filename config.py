9ca746d8bd56112135a5e569a3577c60ab7560dc
maddie <165028446+maddier404@users.noreply.github.com>
2026-04-28 00:18:25 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 COMMAND_PREFIX = "!"
+STARTERS = ["i", "you", "he", "she", "it", "the", "a", "this", "that", "they"]

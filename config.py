ce56cca3ce9a65f387579c47742745b93bf2fc7c
maddie <165028446+maddier404@users.noreply.github.com>
2026-04-28 18:19:21 -0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-COMMAND_PREFIX = "!"
-STARTERS = ["i", "you", "he", "she", "it", "the", "a", "this", "that", "they"]
-# for prompts
-STOPWORDS = {"the", "and", "is", "to", "a", "of"}
